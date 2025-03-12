57cd21c5244588155ce4d94745ebfd53d428ef56
Richard T. Carback III <rick.carback@gmail.com>
2025-03-11 22:57:45 -0400

    Initial test framework w/ code that is close to compiling

--- README.md
+++ README.md
@@ -11,3 +11,32 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat ignition deploy ./ignition/modules/Lock.ts
 ```
+
+## Testing with Foundry
+
+### Installation
+
+1. Install Foundry:
+```bash
+curl -L https://foundry.paradigm.xyz | bash
+foundryup
+```
+
+2. Initialize Foundry in your project:
+```bash
+forge init --no-commit
+```
+
+3. Install dependencies:
+```bash
+forge install foundry-rs/forge-std
+```
+
+### Running Tests
+
+Run the tests with:
+```bash
+forge test -vv
+```
+
+The `-vv` flag increases verbosity to show more test details. Use `-vvv` for even more detailed output including stack traces.
--- contracts/WOTSPlus.sol
+++ contracts/WOTSPlus.sol
@@ -2,16 +2,20 @@
 pragma solidity^0.8.28;
 
 library WOTSPlus {
-    public constant SignatureSize = NumSignatureChunks * HashLen;
-    public constant PublicKeySize = HashLen;
+    // SignatureSize: The size of the signature in bytes.
+    uint8 public constant SignatureSize = NumSignatureChunks * HashLen;
+    // PublicKeySize: The size of the public key in bytes.
+    uint8 public constant PublicKeySize = HashLen * 2;
 
     // Hash: The WOTS+ `F` hash function.
-    public constant Hash = keccak256;
+    function Hash(bytes memory data) internal pure returns (bytes32) {
+        return keccak256(data);
+    }
 
     // HashLen: The WOTS+ `n` security parameter which is the size 
-    // of the `F` hash function output in bytes.
+    // of the hash function output in bytes.
     // This is 32 for keccak256 (256 / 8 = 32)
-    public constant uint8 HashLen = Hash.outputLen;
+    uint8 public constant HashLen = 32;
 
     // MessageLen: The WOTS+ `m` parameter which is the size 
     // of the message to be signed in bytes 
@@ -22,7 +26,7 @@ library WOTSPlus {
     // Note that this is not the message length itself as, like 
     // with most signatures, we hash the message and then compute
     // the signature on the hash of the message.
-    public constant uint8 MessageLen = Hash.outputLen;
+    uint8 public constant MessageLen = 32;
 
     // ChainLen: The WOTS+ `w`(internitz) parameter. 
     // This corresponds to the number of hash chains for each public
@@ -34,16 +38,16 @@ library WOTSPlus {
     // 
     // For XMSS (rfc8391) this value is limited to 4 or 16 because
     // they simplify the algorithm and offer the best trade-offs.
-    public constant uint8 ChainLen = 16;
+    uint8 public constant ChainLen = 16;
     // lg(ChainLen) so we don't calculate it
-    public constant uint8 LgChainLen = 4;
+    uint8 public constant LgChainLen = 4;
 
     // NumMessageChunks: the `len_1` parameter which is the number of
     // message chunks. This is 
     // ceil(8n / lg(w)) -> ceil(8 * HashLen / lg(ChainLen))
     // or ceil(32*8 / lg(16)) -> 256 / 4 = 64
     // Python:  math.ceil(32*8 / math.log(16,2))
-    public constant uint8 NumMessageChunks = 64;
+    uint8 public constant NumMessageChunks = 64;
 
     // NumChecksumChunks: the `len_2` parameter which is the number of
     // checksum chunks. This is
@@ -51,90 +55,126 @@ library WOTSPlus {
     // -> floor(lg(NumMessageChunks * (ChainLen - 1)) / lg(ChainLen)) + 1
     // -> floor(lg(64 * 15) / lg(16)) + 1 = 3
     // Python: math.floor(math.log(64 * 15, 2) / math.log(16, 2)) + 1
-    public constant uint8 NumChecksumChunks = 3;
+    uint8 public constant NumChecksumChunks = 3;
 
-    public constant uint8 NumSignatureChunks = NumMessageChunks + NumChecksumChunks;
+    uint8 public constant NumSignatureChunks = NumMessageChunks + NumChecksumChunks;
 
-    function verify(bytes memory publicKey, bytes memory message, bytes memory signature) public pure returns (bool) {
-        require(publicKey.length == 64, "public key length must be 64 bytes");
+    // verify: Verify a WOTS+ signature. 
+    // 1. The first part of the publicKey is a public seed used to regenerate the randomization elements. (`r` from the paper).
+    // 2. The second part of the publicKey is the hash of the NumMessageChunks + NumChecksumChunks public key segments.
+    // 3. Convert the Message to "base-w" representation (or base of ChainLen representation).
+    // 4. Compute and add the checksum. 
+    // 5. Run the chain function on each segment to reproduce each public key segment.
+    // 6. Hash all public key segments together to recreate the original public key.
+    function verify(bytes calldata publicKey, bytes calldata message, bytes32[NumSignatureChunks] calldata signature) public pure returns (bool) {
+        require(publicKey.length == PublicKeySize, string.concat("public key length must be ", "64", " bytes"));
+        require(message.length == MessageLen, string.concat("message length must be ", "32", " bytes"));
         
-        bytes publicSeed = publicKey[0:32];
-        bytes publicKeyHash = publicKey[32:64];
+        bytes32 publicSeed = bytes32(publicKey[0:HashLen]);
+        bytes32 publicKeyHash = bytes32(publicKey[HashLen:PublicKeySize]);
 
-        bytes[] publicKeySegments = new bytes[](NumMessageChunks + NumChecksumChunks);
+        bytes memory publicKeySegments = new bytes(NumMessageChunks + NumChecksumChunks);
 
-        basewMsg = toBaseW(message, NumMessageChunks);
-        basewChecksum = checksum(basewMsg);
+        // would it be clearer to compute these together in a subfunction, hiding the checksum details entirely?
+        uint8[] memory chainSegments = ComputeMessageHashChainIndexes(message);
 
+        // Compute each public key segment. These are done by taking the signature, which is prevChainOut at chainIdx - 1, 
+        // and completing the hash chain via the chain function to recompute the public key segment.
+        for (uint8 i = 0; i < chainSegments.length; i++ ) {
+            uint8 chainIdx = chainSegments[i];
+            uint8 numIterations = ChainLen - chainIdx - 1;
+            bytes32 prevChainOut = signature[i];
 
-        for (uint i = 0; i < basewMsg.length; i++ ) {
-            uint16 index = basewMsg[i];
-            uint16 numIterations = ChainLen - index - 1;
-            bytes prevChainOut = signature[i];
+            bytes32 segment = chain(prevChainOut, publicSeed, chainIdx, numIterations);
 
-            publicKeySegments[index] = chain(prevChainOut, publicSeed, index, numIterations);
+            publicKeySegments = bytes.concat(publicKeySegments, segment);
         }
 
-        for (uint i = 0; i < basewChecksum.length; i++ ) {
-            uint16 index = basewMsg.length + i;
-            uint16 numIterations = ChainLen - index - 1;
-            bytes prevChainOut = signature[i];
-
-            publicKeySegments[index] = chain(prevChainOut, publicSeed, index, numIterations);
-        }
-
-        // Hash all public key segments together
-        bytes32 computedHash = Hash(abi.encodePacked(publicKeySegments));
+        // Hash all public key segments together to recreate the original public key.
+        bytes32 computedHash = Hash(publicKeySegments);
 
         // Compare computed hash with stored public key hash
-        return computedHash == publicKeyHash;
+        return computedHash == bytes32(publicKeyHash);
     }
 
-    function generateKeyPair(bytes memory privateSeed) public pure returns (bytes memory, bytes memory) {
-        bytes privateKey = prf(privateSeed, 0);
-        bytes publicKey = chain(bytes("0"), privateSeed, 0, ChainLen);
+    // sign: Sign a message with a WOTS+ private key. Do not use this, it is present as an example and
+    // you should be using a typescript version of this function because it requires your private key.
+    function sign(bytes32 privateKey, bytes calldata message) public pure returns (bytes memory) {
+        require(privateKey.length == HashLen, string.concat("private key length must be ", "32", " bytes"));
+        require(message.length == MessageLen, string.concat("message length must be ", "32", " bytes"));
+
+        bytes32 publicSeed = prf(privateKey, 0);
+        bytes32[] memory signature = new bytes32[](NumMessageChunks + NumChecksumChunks);
+
+        uint8[] memory chainSegments = ComputeMessageHashChainIndexes(message);
+
+        for (uint8 i = 0; i < chainSegments.length; i++ ) {
+            uint16 chainIdx = chainSegments[i];
+            bytes32 secretKeySegment = prf(privateKey, i + 1);
+            signature[i] = chain(secretKeySegment, publicSeed, 0, chainIdx);
+        }
+
+        return abi.encodePacked(signature);
+    }
+
+    // generateKeyPair: Generate a WOTS+ key pair. Do not use this, it is present as an example and
+    // you should be using a typescript version of this function, presumably with better entropy source.
+    function generateKeyPair(bytes32 privateSeed) public pure returns (bytes memory, bytes32) {
+
+        bytes32 privateKey = prf(privateSeed, 0);
+        bytes32 publicSeed = prf(privateKey, 0);
+
+        bytes32[] memory publicKeySegments = new bytes32[](NumMessageChunks + NumChecksumChunks);
+        for (uint8 i = 0; i < publicKeySegments.length; i++) {
+            bytes32 secretKeySegment = prf(privateKey, i + 1);
+            publicKeySegments[i] = chain(secretKeySegment, publicSeed, 0, ChainLen - 1);
+        }
+
+        bytes memory publicKey = abi.encodePacked(publicSeed, Hash(abi.encodePacked(publicKeySegments)));
         return (publicKey, privateKey);
-    }
-
-    function sign(bytes memory privateKey, bytes memory message) public pure returns (bytes memory) {
-        return bytes("signature");
     }
 
     // chain is the c_k^i function, 
     // the hash of (prevChainOut XOR randomization element at index).
     // As a practical matter, we generate the randomization elements
     // via a seed like in XMSS(rfc8391) with a defined PRF.
-    function chain(bytes memory prevChainOut, bytes memory publicSeed, uint16 index, uint16 numIterations) public pure returns (bytes memory chainOut) {
+    function chain(bytes32 prevChainOut, bytes32 publicSeed, uint16 index, uint16 steps) private pure returns (bytes32) {
+        // fail fast when out of range
+        // note: maybe this is wasteful since the functions calling this one should never send it?
+        require ((index + steps) < ChainLen, 
+            string.concat("steps + index must be less than ", "16"));
+
+        // Skip the functionKey calculation when it is unneeded. 
+        if (steps == 0) {
+            return prevChainOut;
+        }
 
         // functionKey is `k` from the paper, we define it as the index 0 from the prf, 
         // as the prf output is not used on the first element in the chain function.
         // This is hashed in on each chain iteration along with the randomization element.
         // It is part of the public key, so safe to define it with the public seed.
-        bytes functionKey = prf(publicSeed, 0);
+        // note: maybe worthwhile to calculate this outside this function, e.g., in keygen
+        // off the public seed and/or store it
+        bytes32 functionKey = prf(publicSeed, 0);
     
-        bytes chainOut = prevChainOut;
-        for (uint16 i = 1; i <= numIterations; i++) {
+        bytes32 chainOut = prevChainOut;
+        for (uint8 i = 1; i <= steps; i++) {
             bytes32 randomizationElement = prf(publicSeed, index + i);
-            chainOut = abi.encodePacked(Hash(xor(chainOut, randomizationElement)));
+            chainOut = Hash(abi.encodePacked(functionKey, xor(chainOut, randomizationElement)));
         }
         return chainOut;
     }
 
-    // xor xors 2 byte arrays
-    function xor(bytes memory a, bytes memory b) internal pure returns (bytes memory) {
-        require(a.length == b.length, "Length must be equal");
-        bytes memory result = new bytes(a.length);
-        for (uint i = 0; i < a.length; i++) {
-            result[i] = bytes1(uint8(a[i]) ^ uint8(b[i]));
-        }
-        return result;
-    }    
+    // xor: XOR two bytes32 values
+    function xor(bytes32 a, bytes32 b) internal pure returns (bytes32) {
+        return bytes32(uint256(a) ^ uint256(b));
+    }
 
     // prf: Generate randomization elements from seed and index
     // Similar to XMSS RFC 8391 section 5.1
     // NOTE: while sha256 and ripemd160 are available in solidity,
     // they are implemented as precompiled contracts and are more expensive for gas. 
-    function prf(bytes memory seed, uint16 index) internal pure returns (bytes32) {
+    function prf(bytes32 seed, uint16 index) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(
             bytes1(0x03),  // prefix to domain separate
             seed,          // the seed input
@@ -142,32 +182,64 @@ library WOTSPlus {
         ));
     }
 
+    // ComputeMessageHashChainIndexes: Compute the chain indexes for a message. 
+    // We convert the message to base-w representation (or base of ChainLen representation)
+    // We attach the checksum, also in base-w representation, to the end of the hash chain index list. 
+    function ComputeMessageHashChainIndexes(bytes calldata message) internal pure returns (uint8[] memory) {
+        uint8[] memory chainIndexes = new uint8[](NumMessageChunks + NumChecksumChunks);
+        toBaseW(message, NumMessageChunks, chainIndexes, 0); 
+        checksum(chainIndexes);
+        return chainIndexes;
+    }
+
     // checksum: Calculate the checksum of the message and return it in basew
-    function checksum(uint16[] memory basewMsg) public pure returns (uint16[] memory checksum) {
+    function checksum(uint8[] memory basew) internal pure {
         uint16 csum = 0;
-        for ( i = 0; i < basewMsg.length; i++ ) {
-            csum = csum + ChainLen - 1 - basewMsg[i];
+        for (uint8 i = 0; i < NumMessageChunks; i++ ) {
+            csum = csum + ChainLen - 1 - basew[i];
         }
-        return toBaseW(csum, NumChecksumChunks);
-    }    
 
-    function toBaseW(bytes memory message, uint16 numChunks) public pure returns (uint16[] memory basew) {
-        uint16[] memory basew = new uint16[](numChunks);
-        uint16 in = 0;
-        uint16 out = 0;
-        uint16 total = 0;
-        uint16 bits = 0;
+        // this is left-shifting the checksum to ensure proper alignment when 
+        // converting to base-w representation.
+        // This shift ensures that when we convert to base-w, the least significant
+        // bits of the checksum will be properly aligned with the w-bit boundaries.
+        // (8 - ((NumChecksumChunks * LgChainLen) % 8)) = 4
+        csum = csum << 4;
+        // Per XMSS (rfc8391) this is done in big endian...
+        // It's 2 bytes because thats ceil( ( len_2 * lg(w) ) / 8 ), technically actually 
+        // 12 bits, or 3 basew segments.
+        bytes memory csumBytes = new bytes(2);
+        csumBytes[0] = bytes1(uint8(csum >> 8));    // Most significant byte
+        csumBytes[1] = bytes1(uint8(csum & 0xFF));  // Least significant byte
 
-        for (uint16 consumed = 0; consumed < numChunks; consumed++) {
+        // Convert checksum bytes to base-w and append to basew array
+        toBaseW(csumBytes, NumChecksumChunks, basew, NumMessageChunks);
+    }
+
+    // toBaseW: Convert a message to base-w representation (or base of ChainLen representation)
+    // These numbers are used to index into each hash chain which is rooted at a secret key segment and produces
+    // a public key segment at the end of the chain. Verification of a signature means using these
+    // index into each hash chain to recompute the corresponding public key segment.
+    function toBaseW(bytes memory message, uint8 numChunks, uint8[] memory basew, uint8 offset) internal pure {
+        // Input message index
+        uint8 mIdx = 0;
+        // Output basew index
+        uint8 oIdx = 0 + offset;
+        uint8 total = 0;
+        uint8 bits = 0;
+
+        for (uint8 consumed = 0; consumed < numChunks; consumed++) {
+            // Consume more bits when we run out
             if (bits == 0) {
-                total = message[in];
-                in++;
+                total = uint8(message[mIdx]);
+                mIdx++;
                 bits += 8;
             }
+
+            // Read lg(ChainLen) bits from the message (lg(w))
             bits -= LgChainLen;
-            basew[out] = (total >> bits) & (ChainLen - 1);
-            out++;
+            basew[oIdx] = (total >> bits) & (ChainLen - 1);
+            oIdx++;
         }
-        return basew;
     }
 }
