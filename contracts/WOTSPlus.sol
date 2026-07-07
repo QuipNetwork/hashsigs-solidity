015f1627d8f13b33118774480757f29eee8179c7
Sophia Liu <87018935+sophialiuhk2007@users.noreply.github.com>
2026-07-06 21:09:33 -0400

    update copyright year

--- contracts/WOTSPlus.sol
+++ contracts/WOTSPlus.sol
@@ -1,4 +1,4 @@
-// Copyright (C) 2024 quip.network
+// Copyright (C) 2026 quip.network
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
